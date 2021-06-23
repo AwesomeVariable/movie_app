90aa70033ff6d5890652b7a018a9b3a57edb7275
AwesomeVariable <hyeonseo8312@gmail.com>
2021-06-24 01:56:09 +0900

    Add CSS

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,24 @@
 import PropTypes from "prop-types";
+import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, rating }) {
+function Movie({ year, title, summary, poster, rating, genres }) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{id}</h2>
-      <h3>{rating}</h3>
-      <img src={poster} alt="Movie Poster" />
+    <div className="movies__movie">
+      <img src={poster} alt={title} title={title} />
+
+      <div className="movies__data">
+        <h3 className="movie__title">{title}</h3>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary.slice(0, 200)}...</p>
+        <h4 className="movie__rating">Rating: {rating}</h4>
+      </div>
     </div>
   );
 }
@@ -18,6 +30,7 @@ Movie.propTypes = {
   summary: PropTypes.string.isRequired,
   rating: PropTypes.number.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
